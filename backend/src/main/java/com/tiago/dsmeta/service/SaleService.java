ff95ca53c5e1cb9474cc338ba89236e6e175c636
Tiago <tiagobsb84@gmail.com>
2022-07-13 23:06:20 -0300

    Selecao por Data

--- backend/src/main/java/com/tiago/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/tiago/dsmeta/service/SaleService.java
@@ -1,8 +1,12 @@
 package com.tiago.dsmeta.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.tiago.dsmeta.entities.Sale;
@@ -12,9 +16,15 @@ import com.tiago.dsmeta.repository.SaleRepository;
 public class SaleService {
 
 	@Autowired
-	private SaleRepository saleReposisotry;
+	private SaleRepository saleReposisoty;
 	
-	public List<Sale> listaSales(){
-		return saleReposisotry.findAll();
+	public Page<Sale> listaSales(String minDate, String maxDate, Pageable pageable){
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleReposisoty.findSales(min, max, pageable);
 	}
 }
